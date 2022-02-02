e1290c8e10c04b87c811afe9fd9c968ce4b2d81c
Sebastian <szuviria@gmail.com>
2022-02-02 19:05:36 -0300

    modifique buton del navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,19 @@
 import './NavBar.css'
 
 const NavBar = () => {
-    return (
-      <nav className='NavBar'>
-          <div>
-              <img src={'./images/logo192.png'} alt='logo'/>
-          </div>
-        <button className='Option'>Boton</button>
+    
+  return (
+      <nav className="NavBar">
+        <div>
+            <h3>MARKETapp</h3>
+        </div>
+        <div className="Categories">
+          <button className='Option'>Celulares</button>
+          <button className='Option'>PC</button>
+          <button className='Option'>Notebooks</button>
+        </div>
       </nav>
-    )
+  )
 }
 
 export default NavBar
